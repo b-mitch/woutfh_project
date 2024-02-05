e7dba9b6fba37ea9d0be2707ac410c740d762532
b-mitch <bmitchum.dev@gmail.com>
2024-02-05 15:55:04 -0500

    updates jenkinsfile 1.0.12

--- elb/main.tf
+++ elb/main.tf
@@ -9,8 +9,6 @@ terraform {
     bucket = "woutfh-terraform-alb-remote-state"
     key    = "terraform.tfstate.dev"
     region = "us-east-1"
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
   }
 }
 
